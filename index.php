33be3d7023be5be8d661cc2cecef5d31fe2130b9
whodeveloper <whodevelopthis@gmail.com>
2015-11-07 11:04:38 +1100

    Wong Lye v1.2

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
 <!DOCTYPE html>
 
+
 <html>
 
 	<head>
 		<title>Wong Lye Enterpise</title>
+		<link rel = "stylesheet" type = "text/css" href = "css/style1.css">
 	</head>
 
 	<body>
-		<header>
-			<h3>Wong Lye Enterprise</h3>
 
-			<h2> Base in Singapore </h2>
-		</header>
+		<div class = "fullwrap">
+
+			<div class = "header">
+				<h1> Wong Lye Enterprise</h1>
+			</div>
+
+			<div class = "navbar">
+			</div>
+
+			<div class = "maincontent">
+			</div>
+
+			<div class = "footer">
+			</div>
+
+		</div>
 
 	</body>
 
-
 </html>
+
 
